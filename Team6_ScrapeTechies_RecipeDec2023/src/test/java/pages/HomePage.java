cd03075c07a4e4ac7af6e5182b32e16370125364
twinklejain22 <twinkle.jain.nov@gmail.com>
2023-12-14 06:23:02 +0530

    Twinkle - Modified HomePage class
    Modified Homepage class

--- Team6_ScrapeTechies_RecipeDec2023/src/test/java/pages/HomePage.java
+++ Team6_ScrapeTechies_RecipeDec2023/src/test/java/pages/HomePage.java
@@ -1,17 +1,41 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import managers.FileManager;
+import utilities.Log;
 
 public class HomePage {
 	
 	WebDriver driver;
+	
+	@FindBy(xpath = "//input[contains(@id,'txtsearch')]") WebElement txtSearch;
+	@FindBy(xpath = "//input[@class='searchbtn']") WebElement btnSearch;
+	@FindBy(xpath = "//a[text()='Recipe A To Z']") WebElement linkRecipeAtoZ;
 
 	
 	public HomePage(WebDriver driver)
 	{
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
+	}
+	
+	public void GoToHomePage()
+	{
+		driver.get(FileManager.getInstance().getConfigReader().getWebUrl());
+		Log.info("Navigated to Home Page");
+	}
+	
+	public void SearchRecipe(String txtSearchContent)
+	{
+		txtSearch.sendKeys(txtSearchContent);
+		Log.info("Entered search content in Search textbox");
+		
+		btnSearch.click();
+		Log.info("Clicked on Search button");
 	}
 
 }
--- Team6_ScrapeTechies_RecipeDec2023/src/test/java/testcases/TestClass.java
+++ Team6_ScrapeTechies_RecipeDec2023/src/test/java/testcases/TestClass.java
@@ -1,5 +1,18 @@
 package testcases;
 
-public class TestClass {
+import org.testng.annotations.Test;
+
+public class TestClass extends BaseClass {
+
+	public TestClass() {
+
+	}
+
+	@Test
+	public void Test1() 
+	{
+		homePage.GoToHomePage();
+		homePage.SearchRecipe("PCOS");
+	}
 
 }
